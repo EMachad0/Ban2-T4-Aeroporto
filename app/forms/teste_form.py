12600b5e212539c5f1124313a6a439c7a0412b21
EMachad0 <elitonmachadod200@gmail.com>
2021-07-29 20:45:58 -0300

    forms

--- app/forms/teste_form.py
+++ app/forms/teste_form.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, DateField
+from wtforms import IntegerField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
 
 
