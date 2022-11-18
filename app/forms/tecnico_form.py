e9a08567a938f231d7bfc16f2b4f89d0c8ee8cdc
EMachad0 <elitonmachadod200@gmail.com>
2022-11-18 19:30:55 -0300

    easy setup version

--- app/forms/tecnico_form.py
+++ app/forms/tecnico_form.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, TextField, DecimalField
+from wtforms import IntegerField, StringField, DecimalField
 from wtforms.validators import DataRequired
 
 
 class TecnicoForm(FlaskForm):
     id_fun = IntegerField("Id Funcionário", validators=[DataRequired()])
-    endereco = TextField("Endereço", validators=[DataRequired()])
+    endereco = StringField("Endereço", validators=[DataRequired()])
     telefone = IntegerField("Telefone", validators=[DataRequired()])
     salario = DecimalField("Salario", validators=[DataRequired()])
     n_matricula = IntegerField("Nº Matricula", validators=[DataRequired()])
--- app/forms/tipo_teste_form.py
+++ app/forms/tipo_teste_form.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, TextField
+from wtforms import IntegerField, StringField
 from wtforms.validators import DataRequired
 
 
 class TipoTesteForm(FlaskForm):
     id_tit = IntegerField("Id Tipo do Teste", validators=[DataRequired()])
     n_anac = IntegerField("Nº Anac", validators=[DataRequired()])
-    nome = TextField("Nome", validators=[DataRequired()])
+    nome = StringField("Nome", validators=[DataRequired()])
     pontuacao_maxima = IntegerField("Pontuação Maxima", validators=[DataRequired()])
